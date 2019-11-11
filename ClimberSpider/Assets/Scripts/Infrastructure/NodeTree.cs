dbcdd478449078fa2c1b283012e41d4c04329bd1
Reakain <reakain@users.noreply.github.com>
2019-11-11 16:34:08 -0500

    Generated new BiRRT script as basis for better arm motion attempt

--- ClimberSpider/Assets/Scripts/Infrastructure/NodeTree.cs
+++ ClimberSpider/Assets/Scripts/Infrastructure/NodeTree.cs
@@ -5,10 +5,59 @@ using UnityEngine;
 
 namespace SpiderBot
 {
+
+    public class PointNode
+    {
+        public Vector3 position { get; private set; }
+        public Quaternion rotation { get; private set; }
+        public PointNode parent { get; private set; }
+
+        public List<PointNode> children { get; private set; }
+
+        public bool forwardNode { get; private set; }
+
+        public PointNode(bool forward = true)
+        {
+            parent = null;
+            children = new List<PointNode>();
+            position = new Vector3(0, 0, 0);
+            forwardNode = forward;
+        }
+
+        public PointNode(Vector3 v, bool forward = true)
+        {
+            parent = null;
+            children = new List<PointNode>();
+            position = new Vector3(v.x, v.y, v.z);
+            forwardNode = forward;
+        }
+
+        public PointNode(Vector3 v, PointNode par, bool forward = true)
+        {
+            parent = par;
+            children = new List<PointNode>();
+            position = new Vector3(v.x, v.y, v.z);
+            forwardNode = forward;
+        }
+
+        public void SetParent(PointNode par)
+        {
+            parent = par;
+        }
+
+        public void AddChild(ref PointNode child)
+        {
+            child.SetParent(this);
+            children.Add(child);
+        }
+
+    }
     public class Node
     {
-        public Configuration Point { get; private set; }
         public Node ParentNode { get; private set; }
+
+        public Configuration Point { get; private set; }
+        
         public bool IsGoal { get; private set; }
         private List<float[]> SolutionSteps;
 
@@ -128,8 +177,130 @@ namespace SpiderBot
         }
     }
 
-   
+   public class PointTree
+    {
+        public PointNode rootNode { get; private set; }
+        private bool complete = false;
+        private PointNode finalNode;
+        private int count = 0;
+        bool forwardTree;
 
+        // Debug drawing
+        public float lineWidth = 1f;
+        public bool enableDrawing = true;
+        public Material lineMat;
+        public Color lineColor;
+
+        public void ViewParent(PointNode x)
+        {
+            Debug.Log("Parent is " + x.parent.position);
+        }
+
+        public PointTree(Vector3 pos, bool forward = true)
+        {
+            forwardTree = forward;
+            rootNode = new PointNode(pos, forwardTree);
+        }
+
+        public bool IsComplete()
+        {
+            return complete;
+        }
+
+        public void AddFinalNode(ref PointNode k)
+        {
+            finalNode = k;
+
+            complete = true;
+        }
+
+        public void CreateAndAdd(Vector3 pos)
+        {
+            PointNode nd = new PointNode(pos, forwardTree);
+            PointNode k = GetClosestLeaf(nd.position);
+            AddLeaf(ref k, ref nd);
+        }
+        public PointNode AddLeaf(ref PointNode parentLeaf, ref PointNode childLeaf)
+        {
+            parentLeaf.AddChild(ref childLeaf);
+            if (enableDrawing)
+            {
+
+                DrawLine(childLeaf.position, parentLeaf.position, lineColor);
+            }
+            return childLeaf;
+        }
+        public PointNode GetClosestLeaf(Vector3 pos)
+        {
+            return FindClosestInChildren(rootNode, pos);
+        }
+
+        private PointNode FindClosestInChildren(PointNode x, Vector3 target)
+        {
+            PointNode closest = x, temp;
+            float closestDistance = Vector3.Distance(x.position, target);
+            float checkDistance = 0f;
+            if(x.children.Count != 0)
+            {
+                foreach(PointNode child in x.children)
+                {
+                    temp = FindClosestInChildren(child, target);
+                    checkDistance = Vector3.Distance(temp.position, target);
+                    if(checkDistance < closestDistance)
+                    {
+                        closestDistance = checkDistance;
+                        closest = temp;
+                    }
+                }
+            }
+
+            return closest;
+        }
+
+        public void DrawCompletedPath()
+        {
+            Debug.Log("Drawing Completed Non Recursive");
+            DrawCompletedPath(finalNode);
+            Debug.Log("LINES DRAWN:" + count.ToString());
+        }
+        private void DrawCompletedPath(PointNode x)
+        {
+            Debug.Log("Drawing Completed Recursive");
+            if (!complete)
+            {
+                Debug.Log("Drawing Completed Recursive");
+                return;
+            }
+            if (x.parent == null)
+            {
+                Debug.Log("Parent is Null");
+                return;
+            }
+
+            count++;
+            DrawLine(x.parent.position, x.position, Color.red);
+            DrawCompletedPath(x.parent);
+        }
+
+        private void DrawLine(Vector3 _start, Vector3 _end, Color color)
+        {
+            Vector3 start = new Vector3(_start.x, _start.y, _start.z);
+            Vector3 end = new Vector3(_end.x, _end.y, _end.z);
+
+            GameObject myLine = new GameObject();
+            myLine.transform.position = start;
+            myLine.AddComponent<LineRenderer>();
+            LineRenderer lr = myLine.GetComponent<LineRenderer>();
+            lr.material = lineMat;//new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+            lr.startColor = color;
+            lr.endColor = color;
+            lr.startWidth = lineWidth;
+            lr.endWidth = lineWidth;
+            lr.SetPosition(0, start);
+            lr.SetPosition(1, end);
+            lr.generateLightingData = true;
+        }
+    }
 
     /*
      * Algorithm BuildRRT
---
+++ ClimberSpider/Assets/Scripts/RRT.cs
@@ -0,0 +1,282 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpiderBot {
+    public class RRT : MonoBehaviour
+    {
+        public GameObject target;
+
+        public float xLow, xHigh, yLow, yHigh, zLow, zHigh;
+        public float stepSize, maxIterations, timeStep;
+        public float overlapRadius;
+
+        public bool step = false;
+        public bool disableStepping = false;
+
+        public LayerMask obstacleMask;
+        public LayerMask targetMask;
+
+        private PointTree forwardTree;
+        private PointTree backwardTree;
+        private int counter = 0;
+        private float timer = 0f;
+        private bool goForward = true;
+
+        // Debug drawing
+        public bool enableDrawing = true;
+        public float lineWidth = 1f;
+        public Material lineMat;
+        public Color forwardColor;
+        public Color backwardColor;
+
+        private void Awake()
+        {
+
+        }
+        
+        private void Start()
+        {
+            forwardTree = new PointTree(new Vector3(transform.position.x, transform.position.y, transform.position.z), true);
+            forwardTree.lineWidth = lineWidth;
+            forwardTree.enableDrawing = enableDrawing;
+            forwardTree.lineMat = lineMat;
+            forwardTree.lineColor = forwardColor;
+
+            backwardTree = new PointTree(new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z), false);
+            backwardTree.lineWidth = lineWidth;
+            backwardTree.enableDrawing = enableDrawing;
+            backwardTree.lineMat = lineMat;
+            backwardTree.lineColor = backwardColor;
+        }
+
+        private void Update()
+        {
+            //if (!disableStepping)
+            //{
+            //    if (step)
+            //    {
+            //        timer = timeStep + 1f;
+            //        step = false;
+            //    }
+            //}
+            //else
+            //{
+            //    timer += Time.deltaTime;
+            //}
+
+            if (/*timer > timeStep &&*/ counter < maxIterations && !forwardTree.IsComplete() && !backwardTree.IsComplete())
+            {
+                Vector3 randPoint = GetRandomPoint();
+                Debug.Log("Random Point" + randPoint.ToString());
+                PointNode near;
+                if (goForward)
+                {
+                    near = forwardTree.GetClosestLeaf(randPoint);
+                }
+                else
+                {
+                    near = backwardTree.GetClosestLeaf(randPoint);
+                }
+                
+                //Debug.Log("Nearest Node" + near.position.ToString());
+                Vector3 targPoint;
+
+                if(!IsCollidingObstacle(near.position,randPoint))
+                {
+                    Vector3 direction = randPoint - near.position;
+                    float multiplier = (stepSize < direction.magnitude) ? stepSize : direction.magnitude;
+                    PointNode other = new PointNode(near.position + (direction.normalized * multiplier));
+                    PointNode otherTreeNode;
+                    Vector3 treeTarg;
+                    if(goForward)
+                    {
+                        otherTreeNode = backwardTree.GetClosestLeaf(other.position);
+                        treeTarg = new Vector3(backwardTree.rootNode.position.x, backwardTree.rootNode.position.y, backwardTree.rootNode.position.z);
+                    }
+                    else
+                    {
+                        otherTreeNode = forwardTree.GetClosestLeaf(other.position);
+                        treeTarg = new Vector3(forwardTree.rootNode.position.x, forwardTree.rootNode.position.y, forwardTree.rootNode.position.z);
+                    }
+                    if (IsReachedGoal(near.position,other.position,otherTreeNode.position,treeTarg,out targPoint))
+                    {
+                        if (targPoint == otherTreeNode.position)
+                        {
+                            PointNode finalNode = GetCompositePath(near, otherTreeNode);
+                            if(goForward)
+                            {
+                                forwardTree.AddFinalNode(ref finalNode);
+                                forwardTree.DrawCompletedPath();
+                            }
+                            else
+                            {
+                                backwardTree.AddFinalNode(ref finalNode);
+                                backwardTree.DrawCompletedPath();
+                            }
+                        }
+                        else
+                        {
+                            direction = targPoint - near.position;
+                            multiplier = (stepSize < direction.magnitude) ? stepSize : direction.magnitude;
+                            other = new PointNode(near.position + (direction.normalized * multiplier));
+                            if (goForward)
+                            {
+                                other = forwardTree.AddLeaf(ref near, ref other);
+                                forwardTree.AddFinalNode(ref other);
+                                forwardTree.DrawCompletedPath();
+                            }
+                            else
+                            {
+                                other = backwardTree.AddLeaf(ref near, ref other);
+                                backwardTree.AddFinalNode(ref other);
+                                backwardTree.DrawCompletedPath();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Node Added At " + other.position.ToString());
+                        if (goForward)
+                        {
+                            forwardTree.AddLeaf(ref near, ref other);
+                        }
+                        else
+                        {
+                            backwardTree.AddLeaf(ref near, ref other);
+                        }
+                    }
+                }
+
+                goForward = !goForward;
+
+                //int CollisionInfo = IsColliding(near.position, randPoint, out targPoint);
+
+                //if (CollisionInfo != -1)
+                //{
+                //    if (CollisionInfo == 1)
+                //    {
+                //        Debug.Log("COMPLETE");
+                //        Vector3 direction = targPoint - near.position;
+                //        float multiplier = (stepSize < direction.magnitude) ? stepSize : direction.magnitude;
+                //        PointNode other = new PointNode(near.position + (direction.normalized * multiplier));
+                //        Debug.Log("Node Added At " + other.position.ToString());
+                //        other = forwardTree.AddLeaf(ref near, ref other);
+                //        forwardTree.AddFinalNode(ref other);
+                //        forwardTree.DrawCompletedPath();
+                //    }
+                //    else
+                //    {
+                //        Vector3 direction = randPoint - near.position;
+                //        float multiplier = (stepSize < direction.magnitude) ? stepSize : direction.magnitude;
+                //        PointNode other = new PointNode(near.position + (direction.normalized * multiplier));
+                //        Debug.Log("Node Added At " + other.position.ToString());
+                //        forwardTree.AddLeaf(ref near, ref other);
+                //    }
+                //}
+
+               
+
+
+                    counter++;
+                timer = 0;
+            }
+        }
+
+        int IsColliding(Vector3 from, Vector3 to, out Vector3 targPos)
+        {
+            int temp = 0;
+            targPos = Vector3.zero;
+
+            RaycastHit hit;
+            if (Physics.Raycast(from, (to - from).normalized, out hit, stepSize))
+            {
+                if (hit.collider != null)
+                {
+
+                    if (hit.collider.gameObject.tag == "target")
+                    {
+                        temp = 1;
+                        var targtrans = hit.collider.transform.position;
+                        targPos = new Vector3(targtrans.x, targtrans.y, targtrans.z);
+                    }
+                    else
+                    {
+                        temp = -1;
+                    }
+
+                }
+            }
+
+            return temp;
+        }
+
+        PointNode GetCompositePath(PointNode parent, PointNode child)
+        {
+            PointNode childNew = new PointNode(child.position, parent, goForward);
+            if (goForward)
+            {
+                childNew = forwardTree.AddLeaf(ref parent, ref childNew);
+            }
+            else
+            {
+                childNew = backwardTree.AddLeaf(ref parent, ref childNew);
+            }
+            if(child.parent != null)
+            {
+                return GetCompositePath(childNew, child.parent);
+            }
+            return childNew;
+        }
+
+        bool IsCollidingObstacle(Vector3 from, Vector3 to)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(from, (to - from).normalized, out hit, stepSize, obstacleMask))
+            {
+                if(hit.collider != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool IsReachedGoal(Vector3 from, Vector3 to, Vector3 closestOtherTree, Vector3 targetNode, out Vector3 targetPos)
+        {
+            targetPos = Vector3.zero;
+            RaycastHit hit;
+            if (Physics.Raycast(from, (to - from).normalized, out hit, stepSize, targetMask))
+            {
+                if (hit.collider != null && hit.collider.gameObject.transform.position == targetNode)
+                {
+                    targetPos = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+                    return true;
+                }
+            }
+            else
+            {
+                if(PointCircleCollision(to, closestOtherTree))
+                {
+                    targetPos = new Vector3(closestOtherTree.x, closestOtherTree.y, closestOtherTree.z);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        bool PointCircleCollision(Vector3 from, Vector3 to)
+        {
+            if (Vector3.Distance(from, to) < overlapRadius)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        Vector3 GetRandomPoint()
+        {
+            return new Vector3(Random.Range(xLow, xHigh), Random.Range(yLow, yHigh), Random.Range(zLow,zHigh));
+        }
+    }
+}
